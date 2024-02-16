fa6fe2869407bfe8b00feea610a3c42dda7b13f0
Koliosek <102998869+Koliosek@users.noreply.github.com>
2024-02-16 15:05:24 +0100

    Add files via upload

--- tęcza.py
+++ tęcza.py
@@ -18,20 +18,26 @@ colList = ['purple', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red',]
 #wybór wielkości
 radius = int(input("jaki promień ma mieć tęcza?: "))
 pSize = radius / 10
-step = ((math.pi * radius )/ 180)
-"""
+
 #wybór typu
 print("wpisz 1 dla standardowej tęczy\n")
 print("wpisz 2 dla tęczy z losowymi kolorami\n")
 print("wpisz 3 dla tęczy z dodatkowymi kolorami\n")
 choice = int(input("--> "))
-"""
-choice = 1
+
 t.pensize(pSize)
 
 if(choice == 1):
     for i in range(len(colList)):
+        step = ((math.pi * radius)/ 180)
         bow(radius, step, colList[i] )
         radius = radius + pSize
-        step = ((math.pi * radius)/ 180)
 
+elif(choice == 2):
+    print("nic sie nie dzieje, czekasz z emocjami na dzień w którym to nadejdzie")
+
+elif(choice == 3):
+    print("nic sie nie dzieje, każdy kolejny dzień oczekiwań wydaje sie dłuższy")
+
+else:
+    print("zły numer")
